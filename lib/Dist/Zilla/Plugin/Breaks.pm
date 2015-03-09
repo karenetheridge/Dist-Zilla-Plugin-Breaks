fecb85448e3ba49b45f493290d9625da18ec5273
Karen Etheridge <ether@cpan.org>
2015-03-08 19:29:50 -0700

    fix vim modeline

--- lib/Dist/Zilla/Plugin/Breaks.pm
+++ lib/Dist/Zilla/Plugin/Breaks.pm
@@ -3,7 +3,7 @@ use warnings;
 package Dist::Zilla::Plugin::Breaks;
 # ABSTRACT: Add metadata about potential breakages to your distribution
 # KEYWORDS: distribution metadata prerequisites upstream dependencies modules conflicts breaks breakages
-# vim: set ts=8 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=78 et :
 
 our $VERSION = '0.004';
 
