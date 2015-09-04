cd5211ec1c161f38778a40583220cd3b44de4b35
Karen Etheridge <ether@cpan.org>
2015-09-03 20:40:53 -0700

    this section is now generated by my weaver bundle

--- lib/Dist/Zilla/Plugin/Breaks.pm
+++ lib/Dist/Zilla/Plugin/Breaks.pm
@@ -126,14 +126,6 @@ from within F<Makefile.PL>/F<Build.PL>.
 
 =for Pod::Coverage mvp_multivalue_args metadata
 
-=head1 SUPPORT
-
-=for stopwords irc
-
-Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Plugin-Breaks>
-(or L<bug-Dist-Zilla-Plugin-Breaks@rt.cpan.org|mailto:bug-Dist-Zilla-Plugin-Breaks@rt.cpan.org>).
-I am also usually active on irc, as 'ether' at C<irc.perl.org>.
-
 =head1 SEE ALSO
 
 =for :list
